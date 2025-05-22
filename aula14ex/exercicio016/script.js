112f8e58e936b3b1ae9d85eb5df55e36152dc88c
natan <natannatan123na@gmail.com>
2025-05-21 21:57:18 -0300

    criaçao da func "sequencia"

--- aula14ex/exercicio016/script.js
+++ aula14ex/exercicio016/script.js
@@ -1,4 +1,13 @@
 function sequencia() {
     var inicio = document.getElementById("init")
-    var fim = document.getElementById("")
+    var fim = document.getElementById("fim")
+    var passo = document.getElementById("pass")
+    var resultado = document.getElementById("res")
+    var inicion = Number.parseInt(inicio.value)
+    var fimn = Number.parseInt(fim.value)
+    var passon = Number.parseInt(passo.value)
+    while (inicion <= fimn ){
+        inicion + passon
+        resultado.getElementById = `A sequencia é ${inicion}` 
+    }
 }
